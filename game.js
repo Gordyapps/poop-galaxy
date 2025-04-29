a3f75e8f722296a7dd8b7fcad1e22c8810aaf5ab
Gordyapps <gordyapps@gmail.com>
2025-04-29 07:57:39 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'white';
+ctx.font = '30px Comic Sans MS';
+ctx.fillText('Welcome to Poop Galaxy!', 200, 300);
